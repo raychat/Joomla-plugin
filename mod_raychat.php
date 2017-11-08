b55397f420fc7b145096ec80544a49f48c288ba1
saman <saman.bokan@yahoo.com>
2017-11-08 11:52:13 +0330

    joomla plugin

---
+++ mod_raychat.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+defined('_JEXEC') or die;
+
+$token = $params->get('chatcode');
+
+if(preg_match("/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/", $token)) {
+    JFactory::getDocument()->addScriptDeclaration('!function(){function t(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,localStorage.getItem("rayToken")?t.src="https://app.raychat.io/scripts/js/"+o+"?rid="+localStorage.getItem("rayToken")+"&href="+window.location.href:t.src="https://app.raychat.io/scripts/js/"+o;var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}var e=document,a=window,o="' . $token . '";"complete"==e.readyState?t():a.attachEvent?a.attachEvent("onload",t):a.addEventListener("load",t,!1)}();');
+}
